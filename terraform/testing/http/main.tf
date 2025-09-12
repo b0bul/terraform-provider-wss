e5996540dcf319db9112b6084f29098404aa7647
b0bul <maclighiche@gmail.com>
2025-09-12 17:46:06 +0100

    refactor: rearrange, optional body

--- terraform/testing/http/main.tf
+++ terraform/testing/http/main.tf
@@ -1,23 +1,3 @@
-terraform {
-  required_providers {
-    http = {
-      source  = "hashicorp/http"
-      version = "3.5.0"
-    }
-    assert = {
-      source  = "hashicorp/assert"
-      version = "0.16.0"
-    }
-  }
-}
-
-provider "assert" {}
-provider "http" {}
-
-variable "body" {}
-variable "method" {}
-variable "uri" {}
-variable "endpoint" {}
 
 data "http" "test" {
   url    = "${var.endpoint}${var.uri}"
