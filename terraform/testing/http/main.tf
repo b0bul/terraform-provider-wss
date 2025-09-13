cfbaef7d729d91671f99eeb527be5dfe1c82e5ea
b0bul <me@me>
2025-09-13 12:45:57 +0100

    update: add check block to validate full url

--- terraform/testing/http/main.tf
+++ terraform/testing/http/main.tf
@@ -1,6 +1,17 @@
+locals {
+  url = "${var.endpoint}${var.uri}"
+
+}
+
+check "unique_name" {
+  assert {
+    condition     = can(regex("^https://(?:[a-zA-Z0-9-]+\\.)?[a-zA-Z0-9-]+(?:\\.[a-zA-Z]{2,})+/[a-zA-Z0-9_-]+(?:/[a-zA-Z0-9_-]+)*$", local.url))
+    error_message = format("wants: valid url based on variable validation got: %s", local.url)
+  }
+}
 
 data "http" "test" {
-  url    = "${var.endpoint}${var.uri}"
+  url    = local.url
   method = var.method
   request_headers = {
     Accept = "application/json"
